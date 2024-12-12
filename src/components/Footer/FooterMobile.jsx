503d26f31a6491801882a392cd2fc4d01762c569
mary1457 <piabaldoin@gmail.com>
2024-12-12 17:59:54 +0100

    modifica footer mobile

--- src/components/Footer/FooterMobile.jsx
+++ src/components/Footer/FooterMobile.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 
 const FooterMobile = () => {
   return (
-    <footer className='position-fixed bottom-0 left-0 right-0'>
-    <Container fluid className="bg-dark text-white p-1 d-block d-lg-none">
+   
+    <Container fluid className="bg-dark text-white p-1 d-block d-lg-none position-fixed bottom-0 left-0 right-0">
       <Row className="d-flex justify-content-between text-center">
         <Col xs={3} md={2} className="text-center">
           <Link to={'/home'} className="custom-link-ft">
@@ -34,7 +34,7 @@ const FooterMobile = () => {
         </Col>
       </Row>
     </Container>
-    </footer>
+   
   );
 };
 
